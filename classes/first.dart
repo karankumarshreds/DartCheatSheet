563078cb992450270a8f71923a769e4ba89078f6
Karan Kumar <54062421+karankumarshreds@users.noreply.github.com>
2020-12-09 18:09:47 +0530

    Update first.dart

--- classes/first.dart
+++ classes/first.dart
@@ -14,3 +14,19 @@ class Person {
   }
 }
 
+// same as above but with short hand syntax 
+
+void main() {
+  var person = new Person("Karan Kumar");
+  person.printName();
+}
+
+class Person {
+  String firstName;
+  // same as previous example
+  Person(this.firstName);
+  printName() {
+    print(this.firstName);
+  }
+}
+
