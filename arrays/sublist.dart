e93c304362587216e3f38b3c594209eced0945cd
Karan Kumar <54062421+karankumarshreds@users.noreply.github.com>
2020-12-12 01:01:19 +0530

    Create sublist.dart

---
+++ arrays/sublist.dart
@@ -0,0 +1,8 @@
+/* sublist */
+// Returns a new list containing the elements between start and end
+
+void main() {
+  var myList = ['a', 'b', 'c', 'd'];
+  var newList = myList.sublist(0,2); 
+  print(newList); // returns [a, b]
+}
