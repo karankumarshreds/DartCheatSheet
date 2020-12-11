ab885e5ba960982607fbe1bb0b19cafa3b5ed809
Karan Kumar <54062421+karankumarshreds@users.noreply.github.com>
2020-12-12 00:49:06 +0530

    Update and rename deck-of-cart.dart to deck-of-cards.dart

--- classes/project/deck-of-cards.dart
+++ classes/project/deck-of-cards.dart
@@ -1,8 +1,12 @@
 void main () {
   var deck = new Deck();
   deck.shuffle();
+  var aceCards = deck.cardsWithSuit('Ace');
   for (var card in deck.cards) {
-    print('${card.rank} of ${card.suit}');
+     // print('${card.rank} of ${card.suit}');
+  }
+  for (var aceCard in aceCards) {
+    print('${aceCard.rank} of ${aceCard.suit}');
   }
 }
 
@@ -15,13 +19,21 @@ class Deck {
     for (int i = 0; i < ranks.length; i++ ) {
       for (int j = 0; j < suits.length; j++) {
         var card = new Card(ranks[i], suits[j]);
+        // method to append
         this.cards.add(card);
       }
     }
   }
-  // inbuilt array method 
   shuffle() {
+    // inbuilt array method 
      this.cards.shuffle();
+  }
+  cardsWithSuit(String suit) {
+    // filter type method
+    return this.cards.where((card) {
+      // returns a boolean
+      return card.suit == suit;
+    });
   }
 }
 
