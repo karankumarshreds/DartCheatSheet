6f1373f9632ea9550de3ada1280d654355e855d0
Karan Kumar <54062421+karankumarshreds@users.noreply.github.com>
2020-12-13 01:51:29 +0530

    Update filter.dart

--- arrays/filter.dart
+++ arrays/filter.dart
@@ -3,3 +3,10 @@ void main() {
   var filteredList = myList.where((el) => el != 'c');
   print(filteredList); // returns (a, b, d)
 }
+
+// to mutate the list directly 
+void main() {
+  var myList = ['a', 'b', 'c', 'd'];
+  // mutates the original list
+  myList.removeWhere((el) => el == 'c');
+}
